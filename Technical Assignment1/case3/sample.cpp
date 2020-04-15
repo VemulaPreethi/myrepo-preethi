1d729d8990d2e7f3eb689c61f92dab19fb58e5b7
VemulaPreethi <63638465+VemulaPreethi@users.noreply.github.com>
2020-04-15 12:52:53 +0530

    Create sample.cpp

---
+++ Technical Assignment1/case3/sample.cpp
@@ -0,0 +1,37 @@
+	clude<string.h>
+
+#include<stdlib.h>
+
+using namespace std;
+
+
+
+// global variable 
+
+int iGlobal = 5; 
+
+
+
+// variable accessed from 
+
+inline display() 
+
+{ 
+
+	cout<<iGlobal<<endl; 
+
+} 
+
+
+
+// main function
+
+int main(int argc,char *argv[])
+
+{
+
+    if(argc>2) 
+
+	{
+
+		if(strcmp(argv[1],"-h")==0)     
