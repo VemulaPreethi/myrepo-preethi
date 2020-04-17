2279929da5a4705b4d9a9b2ef5ae39477302d075
VemulaPreethi <63638465+VemulaPreethi@users.noreply.github.com>
2020-04-17 18:53:27 +0530

    Update creatingformula.cpp

--- Technical Assignment1/case2/creatingformula.cpp
+++ Technical Assignment1/case2/creatingformula.cpp
@@ -1,11 +1,17 @@
+/* filename	    : case2.cpp
+   program name	: creating formula for a given series to obtain the output.
+   written by	: preethi
+    date		: 11/04/2020
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
-
+//funtion to calculate
 int sqsum(int n)
 
 {
-
+    //returning the value after calculation
     return(n*(n+1)/2)*(2*n+1)/3;
 }
 
@@ -14,7 +20,8 @@ int main()
 {
 
     int n=4;
-
+    
+    //calling function
     cout<<sqsum(n)<<endl;
 
     return 0;
