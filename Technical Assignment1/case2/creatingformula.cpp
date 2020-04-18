3cf09e0ca51a8d96258d786906907c2aa33da447
VemulaPreethi <63638465+VemulaPreethi@users.noreply.github.com>
2020-04-18 11:12:06 +0530

    Update creatingformula.cpp

--- Technical Assignment1/case2/creatingformula.cpp
+++ Technical Assignment1/case2/creatingformula.cpp
@@ -7,64 +7,42 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-int
-SumofSquares (int inum)
+
+int SumofSquares(int inum)
 {
-
-  int isum;
-
-  //returning the value after calculation
-  isum = (inum * (inum + 1) / 2) * (2 * inum + 1) / 3;
-
-  return isum;
-   
+	int isum;
+	
+	//returning the value after calculation
+  	isum = (inum * (inum + 1) / 2) * (2 * inum + 1) / 3;
+	
+   	return isum;   
 }
 
 int main (int argc, char *argv[])
 {
-
-  int inum;
-  int iresult;
-
-  if (argc == 2)
-
-    {
-      //created a help command
-
-      if (strcmp (argv[1], "-h") == 0)
-
-	    {
-
-	        cout << "Input:Integer" << endl;
-
-	    }
-
-    }
-
-     else
-
-    {
-
-      cout << "enter a number:";
-
-      cin >> inum;
-
-      if (inum > 0)
-
-	    {
-
-	        iresult = SumofSquares (inum);
-
-	        cout << "Output:" << iresult;
-
-        }
-
-      else
-
-	        cout << "Enter Positive Integer" << endl;
-
-    }
-
+	//declaring variables needed
+  	int inum;
+	int iresult;
+	if (argc == 2)
+	{
+		//created a help command
+      		if (strcmp (argv[1], "-h") == 0)
+		{
+	        	cout << "Input:Integer" << endl;
+		}
+	}
+  	else
+    	{
+		cout << "enter a number:";
+      		cin >> inum;
+      		if (inum > 0)
+	    	{
+			//calling function
+	        	iresult = SumofSquares (inum);
+	        	cout << "Output:" << iresult;
+        	}
+     		 else
+		        cout << "Enter Positive Integer" << endl;
+    	}
   return 0;
-
 }
