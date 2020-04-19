0d2482d979666d824e43aec6fd7a9c2e73279d03
VemulaPreethi <preethi.vemula7@gmail.com>
2020-04-19 13:53:39 +0530

    Create storageclasses.cpp

---
+++ Technical Assignment1/case3/storageclasses.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+//declaring variable for extern
+int i_var;
+int Storageclasses()
+{ 
+    //takes no returntype
+    auto a_var=3;
+    
+    //declaring it as extern
+    extern int i_var;
+    //initializing extern variable
+    i_var=5;
+    
+    register char c_var='R';
+    
+    cout<<"--------------------Storage classes------------------"<<endl;
+    
+    cout<<"Auto variable        :    "<<a_var<<endl;
+    cout<<"Extern variable      :    "<<i_var<<endl;
+    cout<<"Register variable    :    "<<c_var<<endl;
+    
+}
+
+// Function containing static variables 
+// memory is retained during execution 
+int staticFun() 
+{ 
+    cout << "For static variables  :    "; 
+    static int count = 0;  
+    count++; 
+    return count; 
+} 
+
+  
+// Function containing non-static variables 
+// memory is destroyed 
+int nonStaticFun() 
+{ 
+    cout<<"For Non-Static variables  :  ";
+  
+    int count = 0; 
+    count++; 
+    return count; 
+} 
+int main(int argc,char *argv[])        // main function
+{
+    if(argc>2) 
+
+	{
+
+		if(strcmp(argv[1],"-h")==0)     //creating help command
+        {
+			cout<<"\n Usage of file --> \n"<<endl;
+			cout<<" Program used to demonstrate types of storage classes"<<endl;
+		}	
+	}
+    else
+    {
+    Storageclasses();
+    
+    // Calling the static parts 
+    cout<<staticFun()<<endl;
+    cout<<staticFun()<<endl;
+  
+    // Calling the non-static parts 
+    cout << nonStaticFun() <<endl;
+    cout << nonStaticFun() <<endl;
+    }
+    return 0;
+    
+    
+}
