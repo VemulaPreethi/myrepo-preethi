f3ea007dad1f91a374bac1d01e420239a2834d78
VemulaPreethi <preethi.vemula7@gmail.com>
2020-04-19 11:46:34 +0530

    Update modifiertypes.cpp

--- Technical Assignment1/case3/modifiertypes.cpp
+++ Technical Assignment1/case3/modifiertypes.cpp
@@ -10,21 +10,37 @@ using namespace std;
 
 void Modifiertypes()
 {
-    signed iSint=-78;
-	  unsigned int iUSint=65479;
-    long double lDou=8347659;
-	  short signed sSint=100;
+	signed iSint=-78;
+	unsigned int iUSint=65479;
+	long double lDou=8347659;
+	short signed sSint=100;
 	
 	cout<<" --------------------MODIFIER TYPES------------------------"<<endl;
-	
-  cout<<"signed int value     : "<<iSint<<endl;
+	cout<<"signed int value     : "<<iSint<<endl;
 	cout<<"unsigned int value   : "<<iUSint<<endl;
 	cout<<"long double value    : "<<lDou<<endl; 
 	cout<<"short signed value   : "<<sSint<<endl;
 }
 
-int main()
+
+int main(int argc,char *argv[])        // main function
 {
-    Modifiertypes();
-    return 0;
+    if(argc>2) 
+	{
+		if(strcmp(argv[1],"-h")==0)     //created help command
+
+        {
+		cout<<"\n Usage of file --> \n"<<endl;
+		cout<<" Program used to demonstrate types of modifiers"<<endl;
+			
+	}	
+
+}
+
+    else
+    {
+
+    	Modifiertypes();
+    	return 0;
+    }
 }
