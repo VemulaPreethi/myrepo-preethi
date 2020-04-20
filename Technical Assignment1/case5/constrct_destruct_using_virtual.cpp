48899dbff6810f16b19912b587fd87e6984c094c
VemulaPreethi <preethi.vemula7@gmail.com>
2020-04-20 21:17:18 +0530

    Update constrct_destruct_using_virtual.cpp

--- Technical Assignment1/case5/constrct_destruct_using_virtual.cpp
+++ Technical Assignment1/case5/constrct_destruct_using_virtual.cpp
@@ -1,37 +1,21 @@
+/*file name	:	construct_destructor_using_virtual.cpp
+  program name	:	Order of construction and destruction of objects using virtual 
+  written by	:	preethi
+  date		:	16/04/2020
+  */
 #include<iostream>
-
 #include<string.h>
-
 using namespace std;
 
-
-
-
-
 class A //class creation
-
 {
-
 public:
-
     A() {
-
-       
-
     }
-
-
-
     virtual void virtual_function()//virtual function 
-
 	{
-
         cout<<"A::virtual_function called"<<endl;
-
     }
-
-
-
     virtual ~A()//virtual destructor 
 
     {
@@ -40,48 +24,23 @@ public:
 
 };
 
-
-
 class B : public A 
-
 {
-
 public:
-
     // override virtual function in A
-
     void virtual_function()
-
     {
-
         cout<<"B::virtual_function called"<<endl;
-
     }
-
 };
 
-
-
 int main(int argc, char **argv)
-
 {
-
-    
-
-     
-
     if(argc == 2 && strcmp(argv[1], "-h")==0) //help command 
-
-   
-
     {
-
         cout<<"The main purpose of this program is to how to use & Declare the Local & Global scope variable"<<endl;
-
     }
-
     else{
-
     // Call to virtual_function during construction doesn't
 
     // behave like normal virtual function call.
@@ -91,7 +50,6 @@ int main(int argc, char **argv)
     // even though we are constructing an instance of B, not A.
 
      B myObject;
-
 
 
     // This call behaves like a normal virtual function call.
