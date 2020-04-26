b51fd9159ab2f71321913ad2d23ee43a8e702af8
VemulaPreethi <preethi.vemula7@gmail.com>
2020-04-26 18:20:53 +0530

    Update const_destruct_operatoroverloading_memberfunctions.cpp

--- Technical Assignment1/case6/const_destruct_operatoroverloading_memberfunctions.cpp
+++ Technical Assignment1/case6/const_destruct_operatoroverloading_memberfunctions.cpp
@@ -5,147 +5,96 @@
   */
 
 #include<iostream>
-
-#include<string.h>
-
-#include<stdio.h>
-
+#include<cstring>
+#include<stdlib.h>
 using namespace std;
-
-
-
-class Student{
-
+class Data
+{
+//private member variables
 private:
-
-    int *iRoll_no;
-
-    char cName[20]; 
-
+	char cName[20];
+	int *iVal;
 public:
+	//default constructor
+	Data()
+	{
+		cout<<"Default constructor invoked"<<endl;
+	}
+	//parametrized constructor
+	Data(char *name)
+	{
+		cout<<"Parametrised constructor invoked"<<endl;
+		strcpy(cName,name);//coping string
+	}
+	//Displaying value
+	void DisplayValue()
+	{
+		cout<<"value: "<<*iVal<<endl;
+	
+	}
+	//displaying name
+	void DisplayName()
+	{
+	    	cout<<"Name: "<<cName<<endl;
+	}
+	
 
-    Student()   
-
-    {
-
-        cout<<"Constructor is Invoked"<<endl;
-
-    }
-
-    Student(int *i_no,char *cname)  
-
-    {
-
-        cout<<"Parameterized Constructor is Invoked"<<endl;
-
-        iRoll_no=i_no;
-
-        strcpy(cName,cname);
-
-    }
-
-    void Assign_Value(int *ino,char cname[])
-
-    {
-
-        iRoll_no=ino;
-
-        strcpy(cName,cname);
-
-    }
-
-    void display()
-
-    {
-
-        cout<<"RollNo:"<<*iRoll_no<<endl<<"Name:"<<cName<<endl;
-
-        cout<<endl;
-
-    }
-
-    void operator ++()  
-
-    {
-
-        ++(*iRoll_no);
-
-    }
-
-    ~Student()
-
-    {
-
-        cout<<"Destructor is Invoked"<<endl;
-
-    }
-
+	//overloading oprator'+'
+	Data operator+(Data s)
+	{
+	    Data temp=cName;
+		strcat(temp.cName,s.cName);	//adding two strings
+		return temp;
+	}
+	//setValue function
+	void setValue(int *inum)
+	{
+		iVal=inum;
+	}
+	//setName function
+/*	void setName(char cname[])
+	{
+		cName=cName;
+	}*/
+	//destructor 
+	~Data()
+	{
+		cout<<"Destructor invoked"<<endl;
+	}
 };
 
-int main(int argc,char* argv[])
-
+int main(int argc,char **argv)
 {
-
-   if(argc>=2)
-
-    {
-
-        if(strcmp(argv[1],"-h")==0)     
-
+	if(argc==2) 
+	{
+		if(strcmp(argv[1],"-h")==0)     //created a help command
         {
-
-            cout<<"used to display private variable int* and char[20], enter one int and char variables"<<endl;
-
-        }
-
-    }
-
-    else
-
-    {
-
-        Student obj;
-
-        int iVal;
-
-        char cNm[20];
-
-        cout<<"Enter RollNo of Student:";
-
-        cin>>iVal;
-
-
-
-        cout<<"Enter Name of Student:";
-
-        getchar();
-
-        cin.get(cNm,100,'\n');
-
-
-
-        cout<<endl<<"Using Parameterized Constructor"<<endl;
-
-        Student obj2(&iVal,cNm);    
-
-        obj2.display();
-
-        cout<<"After Operator Overloading"<<endl;
-
-        ++obj2;     
-
-        cout<<"Using Member Function"<<endl;
-
-        obj.Assign_Value(&iVal,cNm);    
-
-        obj.display();
-
-    }
-
-
-
-    return 0;
-
-
-
+			cout<<"\n Usage of file --> \n"
+					"\t filename.exe & enter"<<endl<<
+					"\t Program used to access pointer to integer and char variables. "<<endl<<
+					"\t insert an integer and any two strings. ";
+		}	
+	}
+	else
+	{	
+		int in; //declaring integer value
+		char first_name[30],last_name[30]; //declaring character value
+		cout<<"enter integer value:";
+		cin>>in;
+		cout<<"First Name: ";
+		cin>>first_name;
+		cout<<"Last Name: ";
+		cin>>last_name;
+		//object declaration for class "Name"
+		Data obj1;
+		Data obj2=(first_name);
+		Data obj3=(last_name);
+		obj1=obj2+obj3;
+		
+		Data obj4;
+		obj4.setValue(&in);
+		
+		obj4.DisplayValue();
+		obj1.DisplayName();
+	}
 }
