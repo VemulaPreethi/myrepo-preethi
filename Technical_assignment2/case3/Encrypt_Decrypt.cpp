36a31aceddabf58a9f5d11361f5cf270546b4b7b
VemulaPreethi <preethi.vemula7@gmail.com>
2020-05-26 16:59:27 +0530

    Update Encrypt_Decrypt.cpp

--- Technical_assignment2/case3/Encrypt_Decrypt.cpp
+++ Technical_assignment2/case3/Encrypt_Decrypt.cpp
@@ -1,3 +1,9 @@
+/*filename      :   "Encrypt_Decrypt.cpp"
+  functionality :    methods declared in the header file are defiined in this 
+                     file.
+  written by    :   preethi
+  date          :   25/05/2020
+  */
 #include "Encrypt_Decrypt.h"
 #include <iostream>
 #include<string.h>
@@ -8,23 +14,38 @@
 //#include<cmath>
 //#include<math.h>
 using namespace std;
-
+//method used for key encryption and decryption
+//using XOR operation
+string encryptdecrypt_key(string toEncrypt) {
+    char key = 'K'; //Any char will work
+    string output = toEncrypt;
+    
+    for (int i = 0; i < toEncrypt.size(); i++)
+        output[i] = toEncrypt[i] ^ key;
+    //returns the encrypt or decrypted key
+    return output;
+}
+//encryption of file function
 bool Encrypt_Decrypt::Encryption(char* ckey,char* filename)
 {
     if(strcmp(filename,"inputfile.txt")==0)
     {
+        //taking alphaneumeric value from the user to fill in space
         cout<<"enter an alphaneumeric character:";
         cin>>calph_neu;
         ifstream myfile ("inputfile.txt");
+        //opening output file
         ofstream outfile ("encrypt.txt");
         
     if (myfile.is_open())
      {
-      outfile<<ckey<<endl;
+     string encryptedkey = encryptdecrypt_key(ckey);
+      outfile<<encryptedkey<<endl;
       outfile<<calph_neu<<endl;
       string line;
       while (! myfile.eof())	
         {
+            //takes line by line
             getline (myfile,line);
             int len=line.length();
             for(icount=0;icount<len;icount++)
@@ -43,8 +64,7 @@ bool Encrypt_Decrypt::Encryption(char* ckey,char* filename)
 	        }
 	        outfile<<line<<endl;
         }//endofwhile
-     
-    myfile.close();
+      myfile.close();
     outfile.close();
     return 1;
      } //endof if
@@ -61,19 +81,22 @@ bool Encrypt_Decrypt::Encryption(char* ckey,char* filename)
     }
 }//endofmethod
 
+//method used for decryption of encrypted file after checking the key and file name
 bool Encrypt_Decrypt::Decryption(char* key,char* filename)
 {
     if(strcmp(filename,"encrypt.txt")==0)
     {
        string line,skey,dummyline,salpha_neumeric,sword;
        string sinput_key=key;
-       ifstream myfile ("encrypt.txt");
-       getline (myfile,skey);
+       ifstream myfile("encrypt.txt");
+       getline(myfile,skey);
+       string decryptedkey = encryptdecrypt_key(skey);
        getline(myfile,salpha_neumeric);
-     
-       if(strcmp(sinput_key.c_str(),skey.c_str())==0)
+        //checking the user entered key with the key set for encryption 
+       if(strcmp(sinput_key.c_str(),decryptedkey.c_str())==0)
        {
         ifstream myfile ("encrypt.txt");
+        //opening output file
         ofstream outfile ("decrypt.txt");
         if (myfile.is_open())
         {
@@ -124,9 +147,3 @@ bool Encrypt_Decrypt::Decryption(char* key,char* filename)
         cout<<"you have entered wrong filename!"<<endl;
     }
 }//end of method
-        
-  
-       
-    
-
-
