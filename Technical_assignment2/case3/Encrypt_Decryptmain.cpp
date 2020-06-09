9eaaa683be54ff274d297803eef0a95a643aa41e
VemulaPreethi <preethi.vemula7@gmail.com>
2020-06-09 11:37:36 +0530

    Update Encrypt_Decryptmain.cpp

--- Technical_assignment2/case3/Encrypt_Decryptmain.cpp
+++ Technical_assignment2/case3/Encrypt_Decryptmain.cpp
@@ -13,23 +13,28 @@ int main(int argc,char *argv[])
 {
     for(int iargcount=1;iargcount<=argc;iargcount++)
     {
+      //creating help command
         if(strcmp(argv[iargcount],"-h")==0)
        {
             cout<<"Enter -e to encrypt data or -d to decrypt data ";
             cout<<"followed by -f [filename] and -k [key] or viceversa ";
             return 0;
         }
+      //checking if the user option is encryption
         else if(strcmp(argv[iargcount],"-e")==0)
         {
-            
+          //checking filename and key
             if((strcmp(argv[iargcount+1],"-f")==0 && strcmp(argv[iargcount+3],"-k")==0))
             {
+              //creating object for the class to proceed encryption
                 Encrypt_Decrypt encryptdecrypt;
                 if(encryptdecrypt.Encryption(argv[iargcount+4],argv[iargcount+2])==1)
               cout<<"Your file has been encrypted"<<endl;
               else
+                //if the file fails to encrypt
               cout<<"Failed to encrypt!"<<endl;  
             }
+          //filename and key can be given viceversa followed by -e
            else if((strcmp(argv[iargcount+3],"-k")==0 && strcmp(argv[iargcount+1],"-f")==0)) 
            {
                
@@ -40,16 +45,21 @@ int main(int argc,char *argv[])
               cout<<"Failed to encrypt!"<<endl;  
            }
         }
+      //checking if the user wants to decrypt the file
         else if(strcmp(argv[iargcount],"-d")==0)
         {
+          //checking for filename and key
          if((strcmp(argv[iargcount+1],"-f")==0 && strcmp(argv[iargcount+3],"-k")==0))
          {
+           //creating object for the class
              Encrypt_Decrypt encryptdecrypt;
              if(encryptdecrypt.Decryption(argv[iargcount+4],argv[iargcount+2])==1)
              cout<<"Your file has been decrypted"<<endl;
              else
+               //if it fails to decrypt
              cout<<"Failed to decrypt!"<<endl; 
          }
+          //user can give filename and key viceversa followed by -d
         else if((strcmp(argv[iargcount+3],"-k")==0 && strcmp(argv[iargcount+1],"-f")==0)) 
         {
             Encrypt_Decrypt encryptdecrypt;
@@ -59,6 +69,7 @@ int main(int argc,char *argv[])
             cout<<"Failed to decrypt!"<<endl;  
          }
         }
+      //if the user option does not match any of the format
         else
         {
             cout<<"you have entered wrong format!!"<<endl;
